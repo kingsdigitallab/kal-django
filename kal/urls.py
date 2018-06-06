b9eb3dee489f9cbddf4bab3aeac1eb20e26889b0
Miguel Vieira <jmvieira@gmail.com>
2018-06-06 15:47:39 +0100

    Enabled LDAP authentication.

--- kal/urls.py
+++ kal/urls.py
@@ -1,20 +1,16 @@
-# from ddhldap.signal_handlers import register_signal_handlers as \
-#     ddhldap_register_signal_handlers
-
+from ddhldap.signal_handlers import \
+    register_signal_handlers as ddhldap_register_signal_handlers
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from kaldb import urls as kaldb_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
 
-
-from kaldb import urls as kaldb_urls
-
 admin.autodiscover()
-# ddhldap_register_signal_handlers()
+ddhldap_register_signal_handlers()
 
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
